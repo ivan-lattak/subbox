0c3b07f9b51ea5811804cc2e1af404f52d944c8d
Ivan Lattak <ivanlattak@gmail.com>
2019-03-17 04:32:18 +0100

    Add `DurationFormatter` tests for negative Durations

--- src/test/java/subbox/util/DurationFormatterTests.java
+++ src/test/java/subbox/util/DurationFormatterTests.java
@@ -21,6 +21,7 @@ class DurationFormatterTests {
 
     private static Stream<Arguments> durationFormatterTestArgs() {
         return Stream.of(
+                arguments(Duration.ZERO, "0ms"),
                 arguments(Duration.ofNanos(0), "0ms"),
                 arguments(Duration.ofNanos(5), "0.000005ms"),
                 arguments(Duration.ofNanos(42_000), "0.042ms"),
@@ -28,7 +29,21 @@ class DurationFormatterTests {
                 arguments(Duration.ofNanos(2_990_000), "2.99ms"),
                 arguments(Duration.ofMillis(4), "4ms"),
                 arguments(Duration.ofMillis(1_800), "1s800ms"),
-                arguments(Duration.ofSeconds(3, 1500), "3s0.0015ms")
+                arguments(Duration.ofSeconds(3).plusNanos(1500), "3s0.0015ms"),
+                arguments(Duration.ofSeconds(15).plusMillis(31), "15s31ms"),
+                arguments(Duration.ofSeconds(180), "3m"),
+                arguments(Duration.ofMinutes(7).plusNanos(16_000_256), "7m16.000256ms"),
+                arguments(Duration.ofMinutes(11).plusSeconds(12), "11m12s"),
+                arguments(Duration.ofMinutes(30).plusSeconds(20).plusMillis(10), "30m20s10ms"),
+                arguments(Duration.ofHours(9).plusMinutes(42), "9h42m"),
+                arguments(Duration.ofHours(12), "12h"),
+                arguments(Duration.ofHours(20).plusSeconds(15).plusNanos(2800), "20h15s0.0028ms"),
+                arguments(Duration.ofHours(25).plusMinutes(75), "1d2h15m"),
+                arguments(Duration.ofNanos(123456789012345678L), "1428d21h33m9s12.345678ms"),
+                arguments(Duration.ZERO.negated(), "0ms"),
+                arguments(Duration.ofNanos(0).negated(), "0ms"),
+                arguments(Duration.ofMillis(65).plusNanos(789).negated(), "-65.000789ms"),
+                arguments(Duration.ofNanos(10644967890123456L).negated(), "-123d4h56m7s890.123456ms")
         );
     }
 
